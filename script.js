a652da105d33396dbae8c5d755b43ccf37b4d01d
bryan.modula <bryan.modula@gmail.com>
2017-04-26 21:49:44 -0400

    stuck on javascript

--- script.js
+++ script.js
@@ -1,10 +1,87 @@
 $(document).ready(function() {
 
-$(".btn").on("click",function(){
-    $(".jumbotron").css({'z-index': -1});
 
+var totalTime = 100;
+var intervalId;
+var totalRight = 0;
+
+
+
+    $(".btn").on("click",function(){
+        $(".jumbotron").hide();
+        $(".hideBox").show();
+        countdown();
+
+
+    });
+
+//countdown timer
+function countdown() {
+    intervalId = setInterval(decrease, 1000);
+}
+
+function decrease() {
+    totalTime--;
+    $("#timer").html("<h1>" + totalTime + "<h2>");
+        if (totalTime === 0) {
+            timeOut();
+            console.log("Times Up");  
+        }
+}
+
+function timeOut(){
+    clearInterval(intervalId);
+}
+
+function answerAdd(){
+
+}
+
+$('.questions input[type="radio"]').click(function(){
+    
+    var type = $(this).data('type'),
+        correctAnswerCount = 
+            $('.questions input[type="radio"]:checked[data-type="1"]').length;
+            if (correctAnswerCount === 1){
+                totalRight++;
+            }
+
+            
+
+    
+    console.log('correctAnswerCount:', correctAnswerCount);
+    console.log("Total Amount Right = ", totalRight , "/10");
 });
 
+
+
+
+
+
+
+
+
+
+//have trivia questions start up after player presses start and timer starts on click
+
+
+
+//have state pictures scrolling under timer
+
+
+
+//if player runs out of time, display you lose
+
+
+//if player finishes game, have it display score on new page
+
+//function to check answers
+
+//submit button button
+
+
+
+//end of document
 });
 
 
@@ -32,7 +109,20 @@ $(function(){
 
 
 
+/*if ($('input[type=radio]:checked').val() === "correct"){
+userScore.correct++;
+}   
+ else {userScore.incorrect++
+        }
+        console.log(userScore)
 
+
+
+
+/*$("input[type=radio]").on("click", function() {
+            corrects = ($(this).val());
+            console.log(obj)
+        });*/
 
 
 
